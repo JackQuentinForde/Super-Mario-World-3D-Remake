0a60013e3db096ebd87099782a2700ad24de9f00
JackQuentinForde <jackforde96@gmail.com>
2023-06-10 12:23:11 +0100

    jump acceleration

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,13 +1,17 @@
 extends CharacterBody3D
 
-const SPEED = 8
+const WALK_SPEED = 8
+const RUN_SPEED = 12
 const TURN_SPEED = 1
-const JUMP_VELOCITY = 8
+const JUMP_ACCEL = 1
+const JUMP_MAX_VELOCITY = 10
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var jumping = false
+var jumpReleased = true
+var speed
 
 # variables that are set in the ready function
 var cameraBasis
@@ -21,13 +25,22 @@ func _ready():
 	cameraBasis.rotation_degrees.y = -90
 
 func _physics_process(delta):
-	# Add the gravity.
+	JumpLogic()
+	
 	if not is_on_floor():
+		speed = WALK_SPEED
+		# apply gravity
 		velocity.y -= gravity * delta
-
+	else:
+		if Input.is_action_pressed("player_sprint"):
+			speed = RUN_SPEED
+		else:
+			speed = WALK_SPEED
+			
 	# Handle Jump.
 	if Input.is_action_just_pressed("player_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_MAX_VELOCITY / 2
+		jumpReleased = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -35,22 +48,22 @@ func _physics_process(delta):
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		direction = direction.rotated(Vector3.UP, cameraBasis.rotation.y).normalized()
-		if velocity.x < direction.x * SPEED:
+		if velocity.x < direction.x * speed:
 			velocity.x += TURN_SPEED
-			if velocity.x > direction.x * SPEED:
-				velocity.x = direction.x * SPEED
-		elif velocity.x > direction.x * SPEED:
+			if velocity.x > direction.x * speed:
+				velocity.x = direction.x * speed
+		elif velocity.x > direction.x * speed:
 			velocity.x -= TURN_SPEED
-			if velocity.x < direction.x * SPEED:
-				velocity.x = direction.x * SPEED
-		if velocity.z < direction.z * SPEED:
+			if velocity.x < direction.x * speed:
+				velocity.x = direction.x * speed
+		if velocity.z < direction.z * speed:
 			velocity.z += TURN_SPEED
-			if velocity.z > direction.z * SPEED:
-				velocity.z = direction.z * SPEED
-		elif velocity.z > direction.z * SPEED:
+			if velocity.z > direction.z * speed:
+				velocity.z = direction.z * speed
+		elif velocity.z > direction.z * speed:
 			velocity.z -= TURN_SPEED
-			if velocity.z < direction.z * SPEED:
-				velocity.z = direction.z * SPEED
+			if velocity.z < direction.z * speed:
+				velocity.z = direction.z * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, TURN_SPEED)
 		velocity.z = move_toward(velocity.z, 0, TURN_SPEED)
@@ -71,3 +84,15 @@ func _physics_process(delta):
 	if velocity.length() > 0.2:
 		var lookDirection = Vector2(velocity.z, velocity.x)
 		mario.rotation.y = lookDirection.angle()
+		
+func JumpLogic():
+	if Input.is_action_pressed("player_jump") and !jumpReleased:
+		if velocity.y < JUMP_MAX_VELOCITY:
+			velocity.y += JUMP_ACCEL
+			if velocity.y >= JUMP_MAX_VELOCITY:
+				velocity.y = JUMP_MAX_VELOCITY
+				jumpReleased = true
+		else:
+			jumpReleased = true
+	else:
+		jumpReleased = true
