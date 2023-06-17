915b58061536e9c33eef3d8cf58de5d7ac70ea4d
JackQuentinForde <jackforde96@gmail.com>
2023-06-17 15:20:24 +0100

    score tracking

--- scripts/KoopaCoin.gd
+++ scripts/KoopaCoin.gd
@@ -3,11 +3,16 @@ extends Area3D
 const VALUE = 1000
 
 var popupText = preload("res://scenes/score_popup.tscn")
+var scoreLabel
+
+func _ready():
+	scoreLabel = $"../Score"
 
 func _on_body_entered(_body):
 	var popup = popupText.instantiate()
 	popup.text = str(VALUE)
 	add_child(popup)
+	scoreLabel.text = str(int(scoreLabel.text) + VALUE)
 	$AudioStreamPlayer.play()
 	$AnimationPlayer.play("disappear")
 
