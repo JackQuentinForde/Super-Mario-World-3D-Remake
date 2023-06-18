45e811b162d80a9b95b3e6d9697fb887e5b26663
JackQuentinForde <jackforde96@gmail.com>
2023-06-18 12:31:11 +0100

    Added sprint and spin jump animations to mario; tweaked run speed and acceleration

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,14 +1,15 @@
 extends CharacterBody3D
 
 const WALK_SPEED = 8
-const RUN_SPEED = 12
+const RUN_SPEED = 16
 const TURN_SPEED = 1
 const AIR_TURN_SPEED = 0.2
-const ACCEL = 0.2
+const ACCEL = 0.18
 const JUMP_ACCEL = 1
 const JUMP_MIN_VELOCITY = 3
 const JUMP_MAX_VELOCITY = 9
 const ANIMATION_RUN_SPEED = 1.5
+const ANIMATION_SPRINT_SPEED = 3
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -43,13 +44,11 @@ func _physics_process(delta):
 func SetMoveSpeed():
 	if Input.is_action_pressed("player_sprint") and is_on_floor():
 		speed = min(speed + ACCEL, RUN_SPEED)
-		animationPlayer.speed_scale = ANIMATION_RUN_SPEED
 	else:
 		if speed > WALK_SPEED:
 			speed = max(speed - ACCEL, WALK_SPEED)
 		elif speed < WALK_SPEED:
 			speed = min(speed + ACCEL, WALK_SPEED)
-		animationPlayer.speed_scale = 1
 		
 func SetTurnSpeed():
 	if not is_on_floor():
@@ -91,7 +90,15 @@ func AnimationLogic():
 	if is_on_floor():
 		jumping = false
 		if velocity.x != 0 or velocity.z != 0:
-			animationPlayer.play("Walk")
+			if speed == RUN_SPEED:
+				animationPlayer.play("Sprint")
+				animationPlayer.speed_scale = ANIMATION_SPRINT_SPEED
+			else:
+				animationPlayer.play("Walk")
+				if Input.is_action_pressed("player_sprint"):
+					animationPlayer.speed_scale = ANIMATION_RUN_SPEED
+				else:
+					animationPlayer.speed_scale = 1
 		else:
 			animationPlayer.play("Idle")
 	else:
