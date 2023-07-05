57bdcbf1069e620957c0e223b7ad0e526772e0e7
JackQuentinForde <jackforde96@gmail.com>
2023-07-05 20:17:45 +0100

    added pipe; breakable blocks now separated into those that can be spun and those that can't

--- scripts/breakable_block.gd
+++ scripts/breakable_block.gd
@@ -1,7 +1,6 @@
 extends StaticBody3D
 
 func _ready():
-	$"Flat Block".visible = false
 	$BreakableBlock.visible = true
 	$GPUParticles3D.emitting = false
 
@@ -9,25 +8,8 @@ func _on_area_3d_area_entered(area):
 	if area.name == "SpinArea":
 		$CollisionShape3D.call_deferred("set_disabled", true)
 		$Area3D/CollisionShape3D2.call_deferred("set_disabled", true)
-		$Area3D2/CollisionShape3D3.call_deferred("set_disabled", true)
 		$AnimationPlayer.play("break")
-			
-func _on_area_3d_2_area_entered(area):
-	if area.name == "HeadArea":
-		$CollisionShape3D.call_deferred("set_disabled", true)
-		$Area3D/CollisionShape3D2.call_deferred("set_disabled", true)
-		$Area3D2/CollisionShape3D3.call_deferred("set_disabled", true)
-		$AnimationPlayer.play("spin")
-		$Timer.start()
 			
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "break":
 		queue_free()
-
-func _on_timer_timeout():
-	$AnimationPlayer.stop()
-	$"Flat Block".visible = false
-	$BreakableBlock.visible = true
-	$CollisionShape3D.call_deferred("set_disabled", false)
-	$Area3D/CollisionShape3D2.call_deferred("set_disabled", false)
-	$Area3D2/CollisionShape3D3.call_deferred("set_disabled", false)
