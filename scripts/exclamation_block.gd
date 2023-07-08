527e47747d4f72e0ae5c075b81635099b2900833
JackQuentinForde <jackforde96@gmail.com>
2023-07-08 11:41:57 +0100

    Exclamation block spawns mushroom

--- scripts/exclamation_block.gd
+++ scripts/exclamation_block.gd
@@ -1,6 +1,10 @@
 extends StaticBody3D
 
+var mushroom = preload("res://scenes/mushroom.tscn")
+
 func _on_area_3d_area_entered(area):
 	if area.name == "HeadArea":
 		$Area3D/CollisionShape3D2.call_deferred("set_disabled", true)
 		$AnimationPlayer.play("bonk")
+		var instance = mushroom.instantiate()
+		add_child(instance)
