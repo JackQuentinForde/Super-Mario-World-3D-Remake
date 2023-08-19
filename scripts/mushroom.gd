ada11448555339bbca266b405be1380e424ab2af
JackQuentinForde <jackforde96@gmail.com>
2023-08-19 14:35:55 +0100

    Fixed mushroom code

--- scripts/mushroom.gd
+++ scripts/mushroom.gd
@@ -3,13 +3,11 @@ extends RigidBody3D
 const VALUE = 1000
 
 var scoreLabel
-var player
 var popup
 var rng = RandomNumberGenerator.new()
 
 func _ready():
 	scoreLabel = $"../../../CanvasLayer/Score"
-	player = $"../../../Player"
 	popup = $"../Popup"
 	popup.visible = false
 	jump()
@@ -20,13 +18,13 @@ func jump():
 	apply_force(Vector3(rand_x, 400, rand_z))
 
 func _on_body_entered(body):
-	if body is CharacterBody3D:
+	if body.name == "Player":
 		collision_mask = 0
 		popup.position = Vector3(position.x, position.y + 2, position.z)
 		popup.visible = true
 		scoreLabel.text = "x " + str(int(scoreLabel.text) + VALUE)
 		$MushroomMesh.visible = false
-		player.call_deferred("ChangeSize", player.SIZE_BIG)
+		body.call_deferred("ChangeSize", body.SIZE_BIG)
 		$Timer.start()
 
 func _on_timer_timeout():
