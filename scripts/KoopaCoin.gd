a8b34da288261dfd63bc2d84403ad5a9a1dd49ba
JackQuentinForde <jackforde96@gmail.com>
2023-09-06 00:16:49 +0100

    created underground level scene

--- scripts/KoopaCoin.gd
+++ scripts/KoopaCoin.gd
@@ -8,15 +8,16 @@ func _ready():
 	scoreLabel = $"../CanvasLayer/Score"
 	$Popup.visible = false
 
-func _on_body_entered(_body):
-	collision_mask = 0
-	$Popup.visible = true
-	scoreLabel.text = "x " + str(int(scoreLabel.text) + VALUE)
-	$AudioStreamPlayer.play()
-	$AnimationPlayer.play("disappear")
+func _on_body_entered(body):
+	if body.name == "Player":
+		collision_mask = 0
+		$Popup.visible = true
+		scoreLabel.text = "x " + str(int(scoreLabel.text) + VALUE)
+		$AudioStreamPlayer.play()
+		$AnimationPlayer.play("disappear")
 
 func _on_animation_player_animation_finished(_anim_name):
-	$KoopaCoin.visible = false
+	$Mesh.visible = false
 	$Timer.start()
 	
 func _on_timer_timeout():
