9ee251a1b84e987ff8a49a10d06a6ba669285f7c
JackQuentinForde <jackforde96@gmail.com>
2023-10-27 18:07:33 +0100

    improved mushroom hitbox

--- scripts/mushroom.gd
+++ scripts/mushroom.gd
@@ -17,7 +17,10 @@ func jump():
 	var rand_z = rng.randf_range(-200.0, 200.0)
 	apply_force(Vector3(rand_x, 400, rand_z))
 
-func _on_body_entered(body):
+func _on_timer_timeout():
+	get_parent().queue_free()
+
+func _on_hit_box_body_entered(body):
 	if body.name == "Player":
 		collision_mask = 0
 		popup.position = Vector3(position.x, position.y + 2, position.z)
@@ -26,6 +29,3 @@ func _on_body_entered(body):
 		$MushroomMesh.visible = false
 		body.call_deferred("ChangeSize", body.SIZE_BIG)
 		$Timer.start()
-
-func _on_timer_timeout():
-	get_parent().queue_free()
