91282dab921b7fcd7f842f0f8d83a277ba2f88f5
JackQuentinForde <jackforde96@gmail.com>
2023-11-13 21:15:17 +0000

    setting coin values

--- scripts/KoopaCoin.gd
+++ scripts/KoopaCoin.gd
@@ -7,6 +7,7 @@ var scoreLabel
 func _ready():
 	scoreLabel = $"../CanvasLayer/Score"
 	$Popup.visible = false
+	SetPopupMaterial()
 
 func _on_body_entered(body):
 	if body.name == "Player":
@@ -15,6 +16,21 @@ func _on_body_entered(body):
 		scoreLabel.text = "x " + str(int(scoreLabel.text) + VALUE)
 		$AudioStreamPlayer.play()
 		$AnimationPlayer.play("disappear")
+
+func SetPopupMaterial():
+	match VALUE:
+		1000: 
+			$Popup.texture = load("res://assets/textures/1000_Texture.png")
+			$Popup.material_override = load("res://materials/1000.tres")
+		2000: 
+			$Popup.texture = load("res://assets/textures/2000_Texture.png")
+			$Popup.material_override = load("res://materials/2000.tres")
+		4000: 
+			$Popup.texture = load("res://assets/textures/4000_Texture.png")
+			$Popup.material_override = load("res://materials/4000.tres")
+		8000: 
+			$Popup.texture = load("res://assets/textures/8000_Texture.png")
+			$Popup.material_override = load("res://materials/8000.tres")
 
 func _on_animation_player_animation_finished(_anim_name):
 	$Mesh.visible = false
