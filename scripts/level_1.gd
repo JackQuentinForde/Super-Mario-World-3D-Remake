933923854bfe091d8f999f60a362c8c63e4fbb16
JackQuentinForde <jackforde96@gmail.com>
2023-11-24 00:23:49 +0000

    Mario HUD

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,5 +1,9 @@
 extends Node3D
 
+func _ready():
+	if get_node("Player").isLuigi:
+		get_node("CanvasLayer/TextureRect2").call_deferred("set_texture", load("res://assets/textures/Luigi HUD.png"))
+
 func HideMountains(isHiding):
 	var mountains = get_tree().get_nodes_in_group("Mountains")
 	
